d9c9e08febad1f61b974484970174ba0a18f6267
Utkarsh Lanjewar <lanjewarutkarsh@gmail.com>
2024-10-16 09:20:45 +0530

    Fixed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,7 @@ import { signOut } from "firebase/auth";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { BsCartDashFill } from "react-icons/bs";
-import {
-  FaSearch,
-  FaSignInAlt,
-  FaSignOutAlt
-} from "react-icons/fa";
+import { FaSearch, FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { auth } from "../firebase";
@@ -31,49 +27,71 @@ const Header = ({ user }: PropsType) => {
   };
 
   return (
-    <nav className="header">
-      <Link onClick={() => setIsOpen(false)} to={"/"}>
-        HOME
-      </Link>
-      <Link onClick={() => setIsOpen(false)} to={"/search"}>
-        <FaSearch size={30} />
-      </Link>
-      <Link onClick={() => setIsOpen(false)} to={"/cart"}>
-        <div style={{position:'relative'}}>
-        <span style={{position:'absolute',bottom:'15px',left:'25px',zIndex:'10',color:'green',fontWeight:'bold',fontSize:'1.5rem'}}>{cartItems.length === 0 ? '' : cartItems.length}</span>
-        <BsCartDashFill size={30}/>
+    <div>
+      <nav className="header">
+        <div className="left-navigation">
+          <Link onClick={() => setIsOpen(false)} to={"/"}>
+            <img src='https://img.icons8.com/?size=100&id=9qeBGQ9SwnO4&format=png&color=000000'/>
+            <span>E-Commerce</span>
+          </Link>
         </div>
-      </Link>
-
-      {user?._id ? (
-        <>
-          <button onClick={() => setIsOpen((prev) => !prev)}>
-            <img src={user.photo} alt="userd_photo"/>
-            {/* <FaUser size={30}/> */}
-          </button>
-          <dialog open={isOpen}>
-            <div>
-              {user.role === "admin" && (
-                <Link onClick={() => setIsOpen(false)} to="/admin/dashboard">
-                  Admin
-                </Link>
-              )}
-
-              <Link onClick={() => setIsOpen(false)} to="/orders">
-                Orders
-              </Link>
-              <button onClick={logoutHandler}>
-                <FaSignOutAlt size={30}/>
-              </button>
+        <div className="right-navigation">
+          <Link onClick={() => setIsOpen(false)} to={"/search"}>
+            <FaSearch size={30} />
+          </Link>
+          <Link onClick={() => setIsOpen(false)} to={"/cart"}>
+            <div style={{ position: "relative" }}>
+              <span
+                style={{
+                  position: "absolute",
+                  bottom: "15px",
+                  left: "25px",
+                  zIndex: "10",
+                  color: "green",
+                  fontWeight: "bold",
+                  fontSize: "1.5rem",
+                }}
+              >
+                {cartItems.length === 0 ? "" : cartItems.length}
+              </span>
+              <BsCartDashFill size={30} />
             </div>
-          </dialog>
-        </>
-      ) : (
-        <Link to={"/login"}>
-          <FaSignInAlt size={30}/>
-        </Link>
-      )}
-    </nav>
+          </Link>
+
+          {user?._id ? (
+            <>
+              <button onClick={() => setIsOpen((prev) => !prev)}>
+                <img src={user.photo} alt="userd_photo" />
+                {/* <FaUser size={30}/> */}
+              </button>
+              <dialog open={isOpen}>
+                <div>
+                  {user.role === "admin" && (
+                    <Link
+                      onClick={() => setIsOpen(false)}
+                      to="/admin/dashboard"
+                    >
+                      Admin
+                    </Link>
+                  )}
+
+                  <Link onClick={() => setIsOpen(false)} to="/orders">
+                    Orders
+                  </Link>
+                  <button onClick={logoutHandler}>
+                    <FaSignOutAlt size={30} />
+                  </button>
+                </div>
+              </dialog>
+            </>
+          ) : (
+            <Link to={"/login"}>
+              <FaSignInAlt size={30} />
+            </Link>
+          )}
+        </div>
+      </nav>
+    </div>
   );
 };
 
