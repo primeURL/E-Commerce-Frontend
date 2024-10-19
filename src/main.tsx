1e8af3cf7d74e25dbc944e85bfa14acf5e906edb
Utkarsh Lanjewar <lanjewarutkarsh@gmail.com>
2024-10-19 20:04:00 +0530

    Added web-analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { Provider } from "react-redux";
 import App from "./App.tsx";
 import { store } from "./redux/store.ts";
 import "./styles/app.scss";
-import { Analytics } from '@vercel/analytics/react';
+import { inject } from '@vercel/analytics';
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
     <Provider store={store}>
       <App />
-      <Analytics/>
     </Provider>
   // </React.StrictMode>
 );
+
+// Initialize Vercel Analytics
+inject();
